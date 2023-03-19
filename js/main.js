79c186e225e889e3ded16715f9e2b05d81c5f779
Binhkio <quan.va205115@gmail.com>
2023-03-19 19:46:07 +0700

    web game

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+$(document).ready(() => {
+
+    $('.main').append('<script src="js/memory-game.js"></script>');
+    $('.main').append('<script src="js/xo-game.js"></script>');
+    $('.main').append('<script src="js/snake-game.js"></script>');
+    
+})
